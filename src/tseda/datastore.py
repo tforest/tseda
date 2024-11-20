cdfe0021fd3cb6ee99f65f322b062680552707b6
Alma Nilsson <aas.nilsson@gmail.com>
2024-11-20 14:39:09 +0100

    Checking sample selection type validity

--- src/tseda/datastore.py
+++ src/tseda/datastore.py
@@ -185,7 +185,10 @@ class IndividualsTable(Viewer):
     @pn.depends("page_size", "sample_select.value", "mod_update_button.value")
     def __panel__(self):
         self.data.rx.value["selected"] = False
-        if self.sample_select.value:
+        if (
+            isinstance(self.sample_select.value, list)
+            and self.sample_select.value
+        ):
             for sample_set_id in self.sample_select.value:
                 self.data.rx.value.loc[
                     self.data.rx.value.sample_set_id == sample_set_id,
