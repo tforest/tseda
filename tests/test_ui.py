e7820dff96962f74d6976f3438e077725e4479f6
hannamlmv <hanna.malmvall@gmail.com>
2024-11-21 16:09:24 +0100

    fix: uncommented tests

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -28,8 +28,8 @@ def test_component(page, port, ds):
     expect(page.get_by_text("predefined")).to_be_visible()
 
     page.get_by_role("button", name="Individuals").click()
-    # expect(page.get_by_text("Data modification")).to_be_visible()
-    # expect(page.get_by_text("Population ID")).to_be_visible()
+    expect(page.get_by_text("Data modification")).to_be_visible()
+    expect(page.get_by_text("Population ID")).to_be_visible()
 
     page.get_by_role("button", name="Structure").click()
     expect(page.get_by_text("GNN cluster plot")).to_be_visible()
