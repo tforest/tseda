89451bfed3780e0e351238844d6d0409173d4e3c
Alma Nilsson <aas.nilsson@gmail.com>
2024-11-25 14:41:55 +0100

    Add empty map when no sets selected

--- src/tseda/vpages/map.py
+++ src/tseda/vpages/map.py
@@ -54,20 +54,47 @@ class GeoMap(View):
         )
         color = color.loc[~gdf.geometry.is_empty.values]
         gdf = gdf[~gdf.geometry.is_empty]
-        return gdf.hvplot.points(
-            hover_cols=["name", "population", "sample_set_id"],
-            geo=True,
-            tiles=self.tiles,
-            tiles_opts={"alpha": 0.5},
-            max_height=401,
-            min_height=400,
-            size=100,
-            color=color,
-            tools=["wheel_zoom", "box_select", "tap", "pan", "reset"],
-            fill_alpha=0.5,
-            line_color="black",
-            responsive=True,
-        )
+        if gdf.empty:
+            gdf = geopandas.GeoDataFrame(
+                df,
+                geometry= geopandas.points_from_xy([0.0], [0.0]),
+                )
+            
+            return gdf.hvplot( #TODO: should make these repeated param values defined in one place
+                geo=True,
+                tiles=self.tiles,
+                tiles_opts={"alpha": 0.5},
+                responsive=True,
+                max_height=200,
+                min_height=199,
+                xlim=(-180, 180),
+                ylim=(-60, 70),
+                tools=["wheel_zoom", "box_select", "tap", "pan", "reset"],
+
+                hover_cols=None,
+                size=100,
+                color=None,
+                fill_alpha=0.0,
+                line_color=None,
+            )
+        else:
+            return gdf.hvplot(
+                geo=True,
+                tiles=self.tiles,
+                tiles_opts={"alpha": 0.5},
+                responsive=True,
+                max_height=200,
+                min_height=199,
+                xlim=(-180, 180),
+                ylim=(-60, 70),
+                tools=["wheel_zoom", "box_select", "tap", "pan", "reset"],
+
+                hover_cols=["name", "population", "sample_set_id"],
+                size=100,
+                color=color,
+                fill_alpha=0.5,
+                line_color="black",
+            )
 
     def sidebar(self):
         return pn.Card(
