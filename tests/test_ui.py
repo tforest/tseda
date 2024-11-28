48666709ffb320252be0178e16dea85159763d5b
ThereseBjorkman <therese.bjorkman@outlook.com>
2024-11-28 13:52:12 +0100

    fix: test unable to go through because of multiple mentions of the same phrase

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -18,7 +18,7 @@ def test_component(page, port, ds):
     )
 
     server = pn.serve(component.view, port=port, threaded=True, show=False)
-    time.sleep(30)
+    time.sleep(50)
     page.goto(url)
 
     page.set_viewport_size({"width": 1920, "height": 1080})
