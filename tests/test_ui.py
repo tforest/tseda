33c6075bfa7b95fc77451067ef61696d42a9d8da
Alma Nilsson <aas.nilsson@gmail.com>
2024-12-11 11:21:55 +0100

    Update test to wait on individuals page

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -18,12 +18,13 @@ def test_component(page, port, ds):
     )
 
     server = pn.serve(component.view, port=port, threaded=True, show=False)
-    time.sleep(150)
+    time.sleep(50)
     page.goto(url)
 
     page.set_viewport_size({"width": 1920, "height": 1080})
 
     page.get_by_role("button", name="Individuals & sets").click()
+    time.sleep(20)
     expect(page.get_by_text("Geomap").nth(0)).to_be_visible()
     expect(page.get_by_text("Original population ID").nth(0)).to_be_visible()
     expect(page.get_by_text("Create new sample set").nth(0)).to_be_visible()
